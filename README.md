2fa276fa32c973067d6d78f64272a8c5cbd9f438
Umar Ashraf <omerashraf7222@gmail.com>
2020-09-26 17:23:34 +0500

    Init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Artenvie-Project
+
---
+++ routes/admin/inbox.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const { authenticateUser } = require('../../helpers/auth');
+const {
+    Inbox,
+    Contact,
+    ChatInboxRoom
+} = require('../../controllers/inbox');
+
+router.all('/*', authenticateUser, (req, res, next) => {
+	req.app.set('layout', 'layouts/admin');
+	next();
+});
+
+
+router.get('/', Inbox);
+
+router.get('/:id/:username', Contact);
+
+router.get('/chat/:id/:reciever', ChatInboxRoom)
+
+
+module.exports = router;
